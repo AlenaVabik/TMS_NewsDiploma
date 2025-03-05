3a658b72405bfa39f5c3f33e95cb59169c0c37e4
Alena Vabishchevich <pilet93@mail.ru>
2025-03-05 22:31:30 +0300

    CreateProject

---
+++ NewsDiploma/NewsDiplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDiplomaApp.swift
+//  NewsDiploma
+//
+//  Created by Alena  on 5.03.25.
+//
+
+import SwiftUI
+
+@main
+struct NewsDiplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
